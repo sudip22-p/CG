d593c4d3e227996fb5708787b4951d446adff7e4
sudip <paudelsudip134@gmail.com>
2023-12-23 17:35:19 +0545

    transformations of triangles added ..

---
+++ 4_transformation/TriangleRotation.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.geom.AffineTransform;
+import java.awt.RenderingHints;
+
+public class TriangleRotation extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    int x1, y1, x2, y2, x3, y3;
+
+    public TriangleRotation() {
+        setTitle("Triangle Rotation");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        initUI();
+    }
+
+    private void initUI() {
+        // Taking input for triangle coordinates
+        String input1 = JOptionPane.showInputDialog("Enter x1 y1 coordinates (separated by space): ");
+        String[] coords1 = input1.split(" ");
+        x1 = Integer.parseInt(coords1[0]);
+        y1 = Integer.parseInt(coords1[1]);
+
+        String input2 = JOptionPane.showInputDialog("Enter x2 y2 coordinates (separated by space): ");
+        String[] coords2 = input2.split(" ");
+        x2 = Integer.parseInt(coords2[0]);
+        y2 = Integer.parseInt(coords2[1]);
+
+        String input3 = JOptionPane.showInputDialog("Enter x3 y3 coordinates (separated by space): ");
+        String[] coords3 = input3.split(" ");
+        x3 = Integer.parseInt(coords3[0]);
+        y3 = Integer.parseInt(coords3[1]);
+
+        // Taking input for rotation angle
+        double angle = Double.parseDouble(JOptionPane.showInputDialog("Enter rotation angle (in degrees): "));
+
+        // Applying rotation to triangle vertices using AffineTransform
+        AffineTransform transform = AffineTransform.getRotateInstance(Math.toRadians(angle), x1, y1);
+        Point p1 = new Point(x1, y1);
+        Point p2 = new Point(x2, y2);
+        Point p3 = new Point(x3, y3);
+        transform.transform(p1, p1);
+        transform.transform(p2, p2);
+        transform.transform(p3, p3);
+
+        // Creating a JPanel for drawing
+        JPanel panel = new JPanel() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                // Enable anti-aliasing
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                drawTriangle(g, x1, y1, x2, y2, x3, y3, Color.BLACK);
+                drawTriangle(g, (int) p1.getX(), (int) p1.getY(), (int) p2.getX(), (int) p2.getY(),
+                        (int) p3.getX(), (int) p3.getY(), Color.GREEN);
+            }
+        };
+
+        getContentPane().add(panel);
+
+        // Display the frame
+        setVisible(true);
+    }
+
+    private void drawTriangle(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3, Color color) {
+        g.setColor(color);
+        int[] xPoints = { x1, x2, x3 };
+        int[] yPoints = { y1, y2, y3 };
+        int nPoints = 3;
+
+        g.drawPolygon(new Polygon(xPoints, yPoints, nPoints));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new TriangleRotation();
+        });
+    }
+}
